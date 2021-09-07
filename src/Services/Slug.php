3039c9d635b6eaccfbea16e3a3bfe2be7778755c
Ibraheem Qanah <i.qanah@gmail.com>
2021-09-07 23:04:27 +0300

    add laravel-random

--- src/Services/Slug.php
+++ src/Services/Slug.php
@@ -22,28 +22,29 @@ class Slug
      */
     public function createSlug($title, $id = 0)
     {
-
-        // Normalize the title
-        $slug = $this->accountSlug($title);
-
-        // Get any that could possibly be related.
-        // This cuts the queries down by doing it once.
-        $allSlugs = $this->getRelatedSlugs($slug, $id);
-
-        // If we haven't used it before then we are all good.
-        if (!$allSlugs->contains('slug', strtolower($slug))) {
-            return $slug;
-        }
-
-        // Just append numbers like a savage until we find not used.
-        for ($i = 1; $i <= 10; $i++) {
-            $newSlug = $slug . '-' . Random::generateString($i, 'abcdefghijklmnopqrstuvwxyz1234567890');
-            if (!$allSlugs->contains('slug', strtolower($newSlug))) {
-                return $newSlug;
-            }
-        }
-
         return '@' . uniqid();
+
+//        // Normalize the title
+//        $slug = $this->accountSlug($title);
+//
+//        // Get any that could possibly be related.
+//        // This cuts the queries down by doing it once.
+//        $allSlugs = $this->getRelatedSlugs($slug, $id);
+//
+//        // If we haven't used it before then we are all good.
+//        if (!$allSlugs->contains('slug', strtolower($slug))) {
+//            return $slug;
+//        }
+//
+//        // Just append numbers like a savage until we find not used.
+//        for ($i = 1; $i <= 10; $i++) {
+//            $newSlug = $slug . '-' . Random::generateString($i, 'abcdefghijklmnopqrstuvwxyz1234567890');
+//            if (!$allSlugs->contains('slug', strtolower($newSlug))) {
+//                return $newSlug;
+//            }
+//        }
+//
+//        return '@' . uniqid();
     }
 
     protected function getRelatedSlugs($slug, $id = 0)
